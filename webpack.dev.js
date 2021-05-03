31a37a55bde7858666b6629f6142d39fe5728855
Adam Wareing <adamwareing241@gmail.com>
2021-05-03 18:55:52 +1200

    Fix dev webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -8,5 +9,5 @@ module.exports = merge(common, {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 8080,
     historyApiFallback: true,
-    },
+  },
 });
