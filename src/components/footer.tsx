c461c8a0a322de890723dd32cdc9ce9ee8a123be
Adam Wareing <adamwareing241@gmail.com>
2021-05-03 18:56:07 +1200

    Add a sponsor footer link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,9 @@ export default class AboutMe extends React.Component<IProps> {
                     <p className='Footer-text'>Adam wareing</p>
                     <div className='Footer-sponsor'>
                         <p className='Footer-sponsor-text'>Blog powered by</p>
-                        <img src={Butter} className='Footer-sponsor-image' />
+                        <a className='Footer-sponsor-link' href='https://buttercms.com'>
+                            <img src={Butter} className='Footer-sponsor-image' />
+                        </a>
                     </div>
                 </div>
             </div>
